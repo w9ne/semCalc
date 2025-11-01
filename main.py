9de7a2ead705107d4b7a18327f2df8cc0b4338e0
jared119@gmail.com <jared119@gmail.com>
2025-10-31 16:36:16 -1000

    Need to fix root main loop so that it takes in code sigh

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import tkinter as tk
 
+#Actual Code
+def calculate():
+    
+    programSponsorFunds = programSponsorFunds.get()
+
+    print(programSponsorFunds)
+
+    programSponsorFunds.set("")
+
+
+# Initialize and Start Window
 prgSponFunds = tk.IntVar()
 personalFunds = tk.IntVar()
 iworkStudentEmployment = tk.IntVar()
 
-
-# Initialize and Start Window
 root = tk.Tk()
 root.title("Financial Semester Calculator")
 root.geometry("600x400")
 
 # Label Program Sponsor Funds
 # This is basically IWORK AMT based off 4 years
-
 programSponsorFunds = tk.Label(root,text='Program Sponor Amount Number', 
 font= (calibre, 10, bold))
-
+#Widget Entry
+prgSponFunds_entry = tk.Entry(root,textvariable = prgSponFunds, 
+font=('calibre',10,'normal'))
 
 root.mainloop()
